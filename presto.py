75b1df9dd5e6a3ffcc8f50163938231bd6c34f22
Alexander Breen <alexander.breen@gmail.com>
2014-08-30 12:55:37 -0400

    Added table of contents extension

--- presto.py
+++ presto.py
@@ -53,7 +53,8 @@ def compute_hash(file):
 
 def main():
     args = {'extensions': ['def_list', 'footnotes', 'meta', 'smarty',
-                           'headerid', mdx_grid_tables.GridTableExtension()],
+                           'headerid', 'toc',
+                           mdx_grid_tables.GridTableExtension()],
             'extension_configs': {'smarty': [('smart_ellipses', False)]},
             'output_format': 'html5',
             'safe_mode': False,         # HTML passes through unaltered
